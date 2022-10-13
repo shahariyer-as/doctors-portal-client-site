f42e2ed9ca97a12467062022cb0d1fd870e6b180
shahariyer.as <shahariyer.as@gmail.com>
2022-10-13 22:58:44 +0600

    3 service section done

--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -1,22 +1,15 @@
 import React from "react";
 
-const Service = () => {
+const Service = ({ service }) => {
   return (
     <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card lg:max-w-lg bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
-          <img
-            src="https://placeimg.com/400/225/arch"
-            alt="Shoes"
-            className="rounded-xl"
-          />
+          <img src={service.img} alt="Shoes" className="rounded-xl" />
         </figure>
         <div className="card-body items-center text-center">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+          <h2 className="card-title">{service.name}</h2>
+          <p>{service.description}</p>
         </div>
       </div>
     </div>
