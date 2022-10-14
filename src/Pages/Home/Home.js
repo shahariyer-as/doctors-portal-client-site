e86a9deea7d3c752042ee4ee7d563149600e7a22
shahariyer.as <shahariyer.as@gmail.com>
2022-10-14 16:07:59 +0600

    testimonals done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from "./Banner";
 import Info from "./Info";
 import MakeAppointment from "./MakeAppointment";
 import Services from "./Services";
+import Testimonial from "./Testimonial";
 
 const Home = () => {
   return (
@@ -11,8 +12,9 @@ const Home = () => {
         <Banner></Banner>
         <Info></Info>
         <Services></Services>
+        <MakeAppointment></MakeAppointment>
+        <Testimonial></Testimonial>
       </div>
-      <MakeAppointment></MakeAppointment>{" "}
     </>
   );
 };
---
+++ src/Pages/Home/Review.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Review = ({ review }) => {
+  return (
+    <div>
+      <div className="card lg:mx-w-lg bg-base-100 shadow-xl text-primary-content">
+        <div className="card-body">
+          <p>{review.reviews}</p>
+          <div className="flex items-center">
+            <div class="avatar">
+              <div class="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
+                <img src={review.img} />
+              </div>
+            </div>
+            <div>
+              <h4 className="text-xl">{review.name}</h4>
+              <p>{review.address}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Review;
