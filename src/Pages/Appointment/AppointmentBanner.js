d3fe7f959ae9b3ca8ba32f106b300df9d282599f
shahariyer.as <shahariyer.as@gmail.com>
2022-10-14 17:07:06 +0600

    state manage appointment calender

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,11 +1,8 @@
-import React, { useState } from "react";
 import chair from "../../assets/images/chair.png";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
-import { format } from "date-fns";
 
-const AppointmentBanner = () => {
-  const [date, setDate] = useState(new Date());
+const AppointmentBanner = ({ date, setDate }) => {
   return (
     <div>
       <div className="hero min-h-screen ">
@@ -17,7 +14,6 @@ const AppointmentBanner = () => {
           />
           <div>
             <DayPicker mode="single" selected={date} onSelect={setDate} />;
-            <p>Selected date: {format(date, "PP")}</p>
           </div>
         </div>
       </div>
---
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -0,0 +1,14 @@
+import { format } from "date-fns";
+import React from "react";
+
+const AvailableAppointments = ({ date }) => {
+  return (
+    <div>
+      <h4 className="text-xl text-secondary text-center">
+        Available Appointment on : {format(date, "PP")}
+      </h4>
+    </div>
+  );
+};
+
+export default AvailableAppointments;
