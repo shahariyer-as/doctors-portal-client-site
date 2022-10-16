49e1aa0103a9473604b499f62a105c5cc11b4aa5
shahariyer.as <shahariyer.as@gmail.com>
2022-10-16 23:55:48 +0600

    send date in backend

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -6,12 +6,12 @@ import Service from "./Service";
 const AvailableAppointments = ({ date }) => {
   const [services, setServices] = useState([]);
   const [treatment, setTreatment] = useState(null);
-
+  const formattedDate = format(date, "PP");
   useEffect(() => {
-    fetch("http://localhost:5000/service")
+    fetch(`http://localhost:5000/available?date=${formattedDate}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [formattedDate]);
   return (
     <div>
       <h4 className="text-xl text-secondary text-center my-12">
