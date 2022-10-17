814cd151ee0d8500925e7d24d514f767751ce9f5
shahariyer.as <shahariyer.as@gmail.com>
2022-10-17 18:00:22 +0600

    mobile responsive dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -5,20 +5,21 @@ const Dashboard = () => {
   return (
     <div>
       <div className="drawer drawer-mobile">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center">
+        <input
+          id="dashboard-sidebar"
+          type="checkbox"
+          className="drawer-toggle"
+        />
+        <div className="drawer-content ">
           {/* <!-- Page content here --> */}
-          <h2 className="text-5xl text-purple-500">Dashboard</h2>
+          <h2 className="text-2xl font-bold text-purple-500">
+            {" "}
+            Well come To Your Dashboard
+          </h2>
           <Outlet></Outlet>
-          <label
-            htmlFor="my-drawer-2"
-            className="btn btn-primary drawer-button lg:hidden"
-          >
-            Open Dashboard
-          </label>
         </div>
         <div className="drawer-side">
-          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
           <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
             {/* <!-- Sidebar content here --> */}
             <li>
